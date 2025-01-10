5390aecb89d2d83af245011ee956e2a57e271aad
jqkwest <edward@hysnq.com>
2025-01-10 15:54:47 -0500

    Added guessing mechanism

--- 100-days-of-code/day-7-hangman/main.py
+++ 100-days-of-code/day-7-hangman/main.py
@@ -1,21 +1,79 @@
 import random
 
-word_list = ["one","two","three","four","five","six"]
+words = ["one","two","three","four","five","six"]
 
-
+hang_state = [
+    "+-------+"
+    "   |    |"
+    "        |"
+    "        |"
+    "        |"
+    "        |",
+     "+-------+"
+    "   |    |"
+    "   O    |"
+    "        |"
+    "        |"
+    "        |",
+    "+-------+"
+    "   |    |"
+    "   O    |"
+    "   |    |"
+    "   |    |"
+    "        |",
+    "+-------+"
+    "   |    |"
+    "   O    |"
+    " --|--  |"
+    "   |    |"
+    "        |",
+    "+-------+"
+    "   |    |"
+    "   O    |"
+    " --|--  |"
+    "   |    |"
+    "  /     |",
+    "+-------+"
+    "   |    |"
+    "   O    |"
+    " --|--  |"
+    "   |    |"
+    "  / \   |"
+]
 
 def main():
     
     empty_char = "_ "
-    word = random.choice(word_list)
+
+    word = random.choice(words)
     word_length = len(word)
+    stil_guessing = True
+    current_guess_state=[]
+    bad_guess_count = 0 
+    
     print("Welcome to hangman... you know what to do!")
-    print("\n" *4)
-    print(word)  # temp for checking the game
 
-    print(empty_char * word_length)
-    print("\n" *4)
+    while stil_guessing:
+        display = ""
 
+        print("\n" *4)
+        print(word)  # temp for checking the game
+
+        guess = input("Please guess a letter: " )
+        for letter in word:
+            if letter == guess:
+                display += letter
+                current_guess_state.append(letter)
+            elif letter in current_guess_state:
+                display += letter
+            else:
+                display += "_ "
+                bad_guess_count += 1 #there is a bug here. It is counting  each iteration istead of just once
+        
+                
+       # print(hang_state[bad_guess_count])
+        print(display)
+        print(bad_guess_count)
 
 
 
